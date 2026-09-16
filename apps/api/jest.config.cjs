aed90c370dbca9423a3c471256c9cb7e4608a165
Mirsosn Belito <mirsonbelitom@gmail.com>
2026-09-16 23:29:28 +0200

    resolvi os testes automatizados e os dados do prisma

---
+++ apps/api/jest.config.cjs
@@ -0,0 +1,7 @@
+﻿module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' }
+};
